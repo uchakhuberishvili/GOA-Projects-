ee60e4c7d99617729a560e73161d46d01a121908
uchakhuberishvili <khuberishviliu@gmail.com>
2024-02-03 22:06:02 +0400

    second commit

---
+++ day2/lesson2.py
@@ -0,0 +1,12 @@
+name = "ucha"
+surname = "khuberishvili"
+print(name)
+print(surname)
+age = "14"
+print(age)
+height = "simagle zustad arvici sadgac 1.78-1.83"
+print(height)
+is_learning_programming = True
+print(is_learning_programming)
+print("kidev ertel davwerot ogond yvela ertad")
+print(name + " " + surname + " " + str(age) + ("wlis") + " " + height + " " + ("da vswavlob programirebas") + ("❤"))
