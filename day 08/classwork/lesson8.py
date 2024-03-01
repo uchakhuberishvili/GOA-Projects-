d1e79c29a5299a0d0edb4075af394d93b8b43910
uchakhuberishvili <khuberishviliu@gmail.com>
2024-03-01 19:00:49 +0400

    eighth commit

---
+++ day 08/classwork/lesson8.py
@@ -0,0 +1,22 @@
+user_input = int(input("please enter your number between 1-100"))
+if user_input == 35:
+    print("you won!")
+else:
+    print("you lost")
+print(True or False)
+is_registered = True
+is_android = True
+print(is_registered and is_android)
+if is_registered == True or is_android:
+    print('access Granted')
+else:
+    print("All options required to be true")
+
+num = int(input("please enter a number" ))
+print(5 % 2 == 0)
+print(num % 2)
+if num > 5 and num % 2 == 0:
+    print("your entered number is > 5 and even")
+else:
+    print("bye")
+    print(5 > 2 and 5 > 3)
---
+++ day 08/homework/lesson8.py
@@ -0,0 +1,40 @@
+#true
+
+temperature = (75)
+housetemp = (85)
+highesttemp = (housetemp > temperature)
+print(highesttemp)
+
+#false
+
+firstdrawer = (10)
+seconddrawer = (20)
+mostspoons = (firstdrawer > seconddrawer)
+print(mostspoons)
+
+#lottery 
+
+user_number = int(input("please enter your number"))
+if user_number == 77:
+    print("you won the lottery!")
+else:
+    print("you lost the lottery!")
+
+#school
+user_school = input("please enter your school")
+if user_school == "GOA":
+    print("congrats!")
+else:
+    print("bro get a life")
+
+#boolean
+    
+a = 300
+b = 55
+user_input = (input("enter your letter"))
+if b > a:
+  print("a is greater than b")
+else:
+  print("b is not greater than a")
+
+
