7c2f4b245ac55a653bd11697dd3cade73770d790
uchakhuberishvili <khuberishviliu@gmail.com>
2024-03-08 18:38:23 +0400

    tenth commit

---
+++ day 010/classwork/homework/lesson10.py
@@ -0,0 +1,38 @@
+#დავალება 1
+
+for i in range(0,21):
+    print(i)
+
+#დავალება 2
+
+user_input1 = int(input("enter your first number"))
+user_input2 = int(input("enter your last number"))
+for i in range(user_input1,user_input2):
+    print (i)
+
+#დავალება 3
+    
+for i in range(50,101):
+    print(i)
+
+#დავალება 4
+    
+for i in reversed (range(50,101)):
+    print(i)
+
+#დავალება 5
+    
+for i in range(0,51):
+    print(i+i)
+
+#დავალება 6
+
+user_input3 = int(input("enter a whole number"))
+for i in range(0,user_input3):
+    print(i)
+
+#დავალება 7
+user_input4 = int(input("enter your age"))
+user_age_10years_later = int(user_input4 + 11)
+for i in range(user_input4,user_age_10years_later):
+    print (i)
---
+++ day 010/classwork/lesson10.py
@@ -0,0 +1,37 @@
+print(range(7,22))
+print(range(0,11))
+print(range(10,20))
+
+for i in range(31):
+    print(i)
+    
+
+for num in range(20):
+    print(num)
+
+for name in range(11):
+    print("ucha")
+
+for i in range(1,31):
+    print(i)
+
+
+for i in range(1,11):
+    print(i * i)
+    
+result = 0
+
+for i in range(1,11):
+    result = result + i
+
+
+result = 1
+for i in range(1):
+    result = result * 1
+ 
+
+
+
+
+
+    
