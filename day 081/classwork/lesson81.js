c336c96e69d0450b05599d8893e382b04220effe
uchakhuberishvili <khuberishviliu@gmail.com>
2024-08-19 16:08:08 +0400

    commit

--- day 081/classwork/lesson81.js
+++ day 081/classwork/lesson81.js
@@ -32,30 +32,4 @@ form.addEventListener('submit', function(event) {
 
 
 
-// const form = document.getElementById('myForm');
-// const btn = document.getElementById('btn');
 
-// const dataBase = [];
-
-// function validateForm() {
-//     const emailValue = form.elements.email.value;
-//     const passValue = form.elements.password.value;
-
-//     if(emailValue == '' || passValue == '') {
-//         alert('Please fill in all fields');
-//         return;
-//     } 
-
-//     const acc = {
-//         email: emailValue,
-//         password: passValue
-//     }
-
-//     dataBase.push(acc);
-
-//     console.log(dataBase)
-
-// }
-
-
-// btn.onclick = validateForm;
---
+++ day 081/homework/lesson81.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('myForm');
+const btn = document.getElementById('btn');
+
+const dataBase = [];
+
+function validateForm() {
+    const emailValue = form.elements.email.value;
+    const passValue = form.elements.password.value;
+    if(emailValue == '' || passValue == '') {
+        alert('Please fill in all fields');
+        return;
+    } 
+
+    const acc = {
+        email: emailValue,
+        password: passValue
+    }
+
+    dataBase.push(acc);
+
+    console.log(dataBase)
+
+}
+
+
+btn.onclick = validateForm;
