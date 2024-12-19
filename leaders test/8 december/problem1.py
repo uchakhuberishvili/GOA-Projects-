078f9418f33f0d4ef49f847e10147a28597eb0d7
uchakhuberishvili <khuberishviliu@gmail.com>
2024-12-19 20:31:57 +0400

    commit

--- leaders test/8 december/problem1.py
+++
@@ -1,9 +0,0 @@
-def positives(arr):
-    res = 0
-    for i in arr:
-        if i > 0:
-            res += i
-    return res
-print(positives([1, -4, 7, 12]))
-print(positives([-1, -2, -3, -4]))
-print(positives([]))
--- leaders test/8 december/problem5.py
+++
@@ -1,6 +0,0 @@
-def anagrams(txt1, txt2):
-    return sorted(txt1) == sorted(txt2)
-
-print(anagrams("listen", "silent"))
-print(anagrams("hello", "world"))
-print(anagrams("triangle", "integral"))
