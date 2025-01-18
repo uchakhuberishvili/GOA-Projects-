4aeca6b5802a1d7ec15a068c1b737186162b311c
uchakhuberishvili <khuberishviliu@gmail.com>
2025-01-19 01:12:16 +0400

    commit

--- mentors test/october/first.py
+++
@@ -1,8 +0,0 @@
-#Given an array containing n-1 numbers taken from the range 1 to n, write a function to find the missing number. There are no duplicates in the array.
-
-def find_missing_number(arr,n):
-    expected_sum = n * (n + 1) // 2
-    actual_sum = sum(arr)
-    missing_number = expected_sum - actual_sum
-    return missing_number
-print(find_missing_number([1,2,3,4,5,7,8,9,10],10))
--- mentors test/october/fourth.py
+++
@@ -1,1 +0,0 @@
-#4. Write a function to return the nth number in the Fibonacci sequence. Solve it both recursively and iteratively.
